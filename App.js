8cae0521a5f45543c28802857419afda35d2f79d
Andrei <rul>
2021-06-16 22:04:56 +0700

    Function vs Class Component

--- App.js
+++ App.js
@@ -1,39 +1,10 @@
-import React,{useState} from 'react';
+import React, {useState} from 'react';
 import type {Node} from 'react';
-import {
-  Text,
-  View, StyleSheet,
-} from 'react-native';
-
-
+import Header from './src/components/uikit/Header';
 
 const App: () => Node = () => {
-  let [title,setTitle] = useState("Hello world");
-  return (
-    <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>{title}</Text>
-    </View>
-  );
+  let [title, setTitle] = useState('Hello world');
+  return <Header title={title} />;
 };
-
-const styles = StyleSheet.create({
-  viewStyle: {
-    backgroundColor: '#30defe',
-    height: 116,
-    justifyContent: 'center',
-    paddingLeft: 22,
-    paddingTop: 71,
-    shadowColor: '#000',
-    shadowOffset: {width: 0, height: 2},
-    shadowOpacity: 0.2,
-    elevation: 2,
-    position: 'relative',
-  },
-  textStyle: {
-    color: '#fff',
-    fontSize: 28,
-    fontFamily: 'AvenirNext-DemiBold',
-  },
-});
 
 export default App;
---
+++ src/components/uikit/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const Header = ({title}) => {
+  return (
+    <View style={styles.viewStyle}>
+      <Text style={styles.textStyle}>{title}</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  viewStyle: {
+    backgroundColor: '#30defe',
+    height: 116,
+    justifyContent: 'center',
+    paddingLeft: 22,
+    paddingTop: 71,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative',
+  },
+  textStyle: {
+    color: '#fff',
+    fontSize: 28,
+    fontFamily: 'AvenirNext-DemiBold',
+  },
+});
+
+export default Header;
