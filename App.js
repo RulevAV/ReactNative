0645a03a7d0b9ffa9f46f118f97f6cc15f4ea6cc
Andrei <rul>
2021-06-16 23:14:58 +0700

    ImageCard

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import React, {useState} from 'react';
 import type {Node} from 'react';
 import Header from './src/components/uikit/Header';
+import {View} from 'react-native';
+import ImageCard from './src/components/ImageCard';
 
 const App: () => Node = () => {
   let [title, setTitle] = useState('Hello world');
-  return <Header title={title} />;
+  return (
+      <View>
+      <Header title={title} />
+        <ImageCard/>
+
+      </View>
+  );
 };
 
 export default App;
---
+++ src/components/ImageCard.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
+import {w} from './Constants';
+import i from '../../ava-twitch-32.jpg';
+const ImageCard = () => {
+  const {h1, cover, container, sub} = styles;
+  return (
+    <View style={container}>
+      <View style={sub}>
+        <Image style={cover} source={i} />
+      </View>
+      <Text style={h1}>asdwasd</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'gold',
+    width: w / 2.1,
+  },
+  sub: {
+    padding: 10,
+    shadowColor: '#000',
+    shadowRadius: 8,
+    shadowOffset: {width: 0, height: 5},
+    shadowOpacity: 0.4,
+  },
+  h1: {
+    fontFamily: 'AvenirNext-DemiBold',
+    fontSize: 18,
+    alignSelf: 'center',
+    textAlign: 'center',
+    width: w / 2.4,
+  },
+  cover: {
+    width: w / 2.4,
+    height: w * 0.63,
+    borderRadius: 10,
+  },
+});
+export default ImageCard;
