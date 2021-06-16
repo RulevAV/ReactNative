1b903378c62634a2196c0e88b16a62d8b0b79296
Andrei <rul>
2021-06-16 21:24:09 +0700

    Стилизация Header

--- App.js
+++ App.js
@@ -1,60 +1,37 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import type {Node} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
   Text,
-  useColorScheme,
-  View,
+  View, StyleSheet,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
 
 const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Hello world</Text>
+    <View style={styles.viewStyle}>
+      <Text style={styles.textStyle}>Hello world</Text>
     </View>
   );
 };
-/*container: {
-  flex:1,
-      justifyContent:'center',
-      alignItems:'center',
-      backgroundColor: '#f5fcff'
-},*/
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  viewStyle: {
+    backgroundColor: '#30defe',
+    height: 116,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'pink',
+    paddingLeft: 22,
+    paddingTop: 71,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative',
   },
-  welcome: {
-    backgroundColor: 'gold',
+  textStyle: {
+    color: '#fff',
+    fontSize: 28,
+    fontFamily: 'AvenirNext-DemiBold',
   },
 });
 
